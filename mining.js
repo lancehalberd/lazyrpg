b84ddcc65b05c259f299caddb45b9560246bf24a
Chris Brewer <chrisb@ning.com>
2014-11-28 13:42:37 -0600

    move mining items into the mining script.

--- mining.js
+++ mining.js
@@ -31,6 +31,96 @@ minerals.silver = {
 //populate monster.key for all monsters
 $.each(minerals, function (key, value) { value.key = key;});
 
+items.copperOre = {
+    'name': 'Copper Ore',
+    'helpText': 'Raw copper ore.',
+    'value': 10
+};
+items.copperIngot = {
+    'name': 'Copper Ingot',
+    'helpText': 'Refined copper that can be used for crafting.',
+    'value': 150
+};
+items.tin = {
+    'name': 'Tin',
+    'helpText': 'Raw tin.',
+    'value': 15
+};
+items.bronzePlating = {
+    'name': 'Bronze Plating',
+    'helpText': 'An alloy of copper and tin suitable for crafting armor',
+    'value': 200
+};
+items.ironOre = {
+    'name': 'Iron Ore',
+    'helpText': 'Raw iron ore.',
+    'value': 25
+};
+items.ironIngot = {
+    'name': 'Iron Ingot',
+    'helpText': 'Refined iron that can be used for crafting.',
+    'value': 300
+};
+items.steelPlating = {
+    'name': 'Steel Plating',
+    'helpText': 'Plates of an alloy of iron and carbon for crafting armor.',
+    'value': 1600
+};
+items.silverOre = {
+    'name': 'Silver Ore',
+    'helpText': 'Raw silver ore.',
+    'value': 80
+};
+items.silverIngot = {
+    'name': 'Silver Ingot',
+    'helpText': 'Refined silver that can be used for crafting, but it isn\'t very hard.',
+    'value': 1000
+};
+items.steeledSilver = {
+    'name': 'Steeled Silver',
+    'helpText': 'A stronger silver alloy. Weaker than regular steel but it repels evil.',
+    'value': 1500
+};
+items.mithrilSilver = {
+    'name': 'Mithril Silver',
+    'helpText': 'A silver alloy that is even stronger than steel.',
+    'value': 4000
+};
+items.goldOre = {
+    'name': 'Gold Ore',
+    'helpText': 'Raw gold ore.',
+    'value': 200
+};
+items.goldIngot = {
+    'name': 'Gold Ingot',
+    'helpText': 'Refined gold that can be used for crafting. It is quite soft for a metal.',
+    'value': 1200
+};
+items.platinumOre = {
+    'name': 'Platinum Ore',
+    'helpText': 'Rare platinum ore.',
+    'value': 1000
+};
+items.platinumIngot = {
+    'name': 'Platinum Ingot',
+    'helpText': 'Refined platinum that can be used for crafting. Somewhat harder than iron.',
+    'value': 6000
+};
+items.titanimOre = {
+    'name': 'Titanium Ore',
+    'helpText': 'Raw titanium ore.',
+    'value': 5000
+};
+items.adamantiumOre = {
+    'name': 'Adamantium Ore',
+    'helpText': 'Raw adamantium ore.',
+    'value': 20000
+};
+items.quartz = {
+    'name': 'Quartz',
+    'value': 14
+};
+
 actions.mine = function (params, successCallback, errorCallback) {
     checkParams(1, params);
     var mineralKey = params[0];
